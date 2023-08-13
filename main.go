dabb1c435128c586de7e64906d79079faab2d499
techby9jo <rattapon.rung@gmail.com>
2023-08-13 22:05:23 +0700

    change main.go

--- main.go
+++ main.go
@@ -31,3 +31,13 @@ func Arrays() {
 	fmt.Println(len(arr3))
 
 }
+func Arrays2() {
+	var arr1 = [3]int{1, 2, 3}
+	arr2 := [5]int{4, 5, 6, 7, 8}
+	arr3 := []string{"rattapon", "rattana"}
+
+	fmt.Println(arr1)
+	fmt.Println(arr2[2])
+	fmt.Println(len(arr3))
+
+}
